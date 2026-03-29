a340041654b7a80b47f3f41883085bd67bf28aeb
squidfunk <martin.donath@squidfunk.com>
2026-03-29 10:21:16 +0200

    Remove unnecessary dependencies

--- setup.py
+++ setup.py
@@ -84,10 +84,12 @@ setup(
     packages=find_packages(exclude=['test', 'test.*']),
     include_package_data=True,
 
-    install_requires=(['importlib_metadata;python_version<"3.10"',
-                       'importlib_resources;python_version<"3.10"',
-                       'jinja2 >= 2.7', 'mkdocs >= 1.0', 'pyparsing >= 3.0',
-                       'pyyaml >= 5.1', 'pyyaml_env_tag', 'verspec']),
+    install_requires=([
+        'jinja2 >= 2.7',
+        'pyparsing >= 3.0',
+        'verspec',
+        'zensical >= 0.0.30',
+    ]),
     extras_require={
         'dev': ['coverage', 'flake8 >= 3.0', 'flake8-quotes', 'shtab'],
         'test': ['coverage', 'flake8 >= 3.0', 'flake8-quotes', 'shtab'],
